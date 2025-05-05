79be02c4ee604cebb4a518c95bc890348d04c7fc
NatanielEscudero <130791079+NatanielEscudero@users.noreply.github.com>
2025-05-05 17:45:26 -0600

    Add files via upload

---
+++ Formularios_js4/Ejercicio_1/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const personas = [];
+const PORT = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+    res.render("index");  
+});
+
+app.post("/submit", (req, res) => {
+    const persona = {
+        usr: req.body.usr,
+        pass: req.body.pass
+    };
+    personas.push(persona);  
+    res.send('Persona agregada <a href="/">Volver</a>');
+});
+
+app.get("/persona", (req, res) => {
+    res.render("persona", { personas });  
+});
+
+app.listen(PORT, '127.0.0.1', () => {
+    console.log(`Listening on 127.0.0.1:${PORT}`);
+});
