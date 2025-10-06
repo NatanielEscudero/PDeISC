86af93cd641bbdf97f326ba04626b57747ee062e
Nataniel Escudero <130791079+NatanielEscudero@users.noreply.github.com>
2025-10-06 18:22:12 -0300

    Update index.js

--- react/R4/backend/index.js
+++ react/R4/backend/index.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
+app.use(cors({
+  origin: 'https://p-de-isc.vercel.app'
+}));
 app.use(express.json());
+
 
 // Conexión a MongoDB Atlas
 mongoose.connect(process.env.MONGO_URI)
@@ -21,3 +25,4 @@ app.use("/api/projects", projectsRouter);
 // Puerto
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
+
