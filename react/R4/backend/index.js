93b76c447e86bdc5a050699e92b639e1ed095437
Nataniel Escudero <130791079+NatanielEscudero@users.noreply.github.com>
2025-10-06 18:26:43 -0300

    Update index.js

--- react/R4/backend/index.js
+++ react/R4/backend/index.js
@@ -1,28 +1,2 @@
-import express from "express";
-import mongoose from "mongoose";
-import cors from "cors";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-const app = express();
-app.use(cors());
-app.use(cors({
-  origin: 'https://p-de-isc.vercel.app'
-}));
-app.use(express.json());
-
-
-// Conexión a MongoDB Atlas
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("✅ Conectado a MongoDB"))
-  .catch(err => console.error("❌ Error MongoDB:", err));
-
-// Rutas
-import projectsRouter from "./routes/projects.js";
-app.use("/api/projects", projectsRouter);
-
-// Puerto
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`));
 
